73a7e7899c1512c7a1b8559afa8797a502a7868e
Chanterelly <zvezdolom1@gmail.com>
2019-08-10 12:23:36 +0900

    exception_count

--- except_func.py
+++ except_func.py
@@ -1,5 +1,8 @@
+exception_count = 0
+
 def print_exception(exeption):
     print('----------------------------------------------------------------------------------')
     print(exeption)
+    exception_count=+1
     print('----------------------------------------------------------------------------------')
     input("Print enter to continue")
