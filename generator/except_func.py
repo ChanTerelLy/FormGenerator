ab47ef0b848bbf3b40f269a8fc52c8d590b5a26f
Chanterelly <zvezdolom1@gmail.com>
2019-08-22 13:26:35 +0900

    and log and timer wrap

--- generator/except_func.py
+++ generator/except_func.py
@@ -1,5 +1,33 @@
+from functools import wraps
+
 def print_exception(exeption):
     print('----------------------------------------------------------------------------------')
     print(exeption)
     print('----------------------------------------------------------------------------------')
     input("Press enter to continue")
+
+def my_logger(orig_func):
+    import logging
+    logging.basicConfig(filename='{}.log'.format(orig_func.__name__), level=logging.INFO)
+
+    @wraps(orig_func)
+    def wrapper(*args, **kwargs):
+        logging.info(
+            'Ran with args: {}, and kwargs: {}'.format(args, kwargs))
+        return orig_func(*args, **kwargs)
+
+    return wrapper
+
+
+def my_timer(orig_func):
+    import time
+
+    @wraps(orig_func)
+    def wrapper(*args, **kwargs):
+        t1 = time.time()
+        result = orig_func(*args, **kwargs)
+        t2 = time.time() - t1
+        print('{} ran in: {} sec'.format(orig_func.__name__, t2))
+        return result
+
+    return wrapper
